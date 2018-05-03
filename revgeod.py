6384853c484c48d97db68f80b7c603866c2157e7
Christoph Krey <krey.christoph@gmail.com>
2018-05-03 19:19:35 +0200

    [NEW] add a basic stats interface

--- revgeod.py
+++ revgeod.py
@@ -12,17 +12,55 @@ from opencage.geocoder import OpenCageGeocode
 import redis
 import paho.mqtt.publish as mqtt
 from creds import *
+import datetime
 
 reverse_timeout = 4
 precision = 8
 
 geocoder = OpenCageGeocode(apikey)  # from creds.py
 
+class Stat(object):
+    def __init__(self, names):
+        self.start = datetime.datetime.utcnow()
+        self.counter = {}
+        for name in names:
+            self.counter[name] = 0
+
+    def reset(self):
+        for name in self.counter.keys():
+            self.counter[name] = 0
+
+    def count(self, name):
+        counter = 0
+        if name in self.counter:
+            counter = self.counter[name]
+        counter += 1
+        self.counter[name] = counter
+
+    def result(self, name):
+        counter = 0
+        if name in self.counter:
+            counter = self.counter[name]
+        return counter
+
+my_stat = Stat(['rev', 'stats', 'geohash_failed', 'cache_ok', 'cache_failed', 'geocode_ok', 'geocode_failed'])
 
 bottle.debug(True)
+@get('/stats')
+def stats():
+    my_stat.count('stats')
+    hit_rate = 0.0
+    if my_stat.result('rev') != 0:
+       hit_rate = my_stat.result('cache_ok') / my_stat.result('rev') * 100.0
+
+    return json.dumps({
+        'uptime': (datetime.datetime.utcnow() - my_stat.start).total_seconds(),
+        'counter': my_stat.counter
+    })
 
 @get('/rev')
 def rev():
+    my_stat.count('rev')
     lat = float(request.query['lat'])
     lon = float(request.query['lon'])
 
@@ -31,6 +69,7 @@ def rev():
     try:
         geohash = Geohash.encode(lat, lon, precision=precision)
     except:
+        my_stat.count('geohash_failed')
         return None
 
     print "{lat}, {lon} = {geohash}".format(lat=lat, lon=lon, geohash=geohash)
@@ -55,17 +94,21 @@ def rev():
             village = address[0]['formatted']
 
             r.set(key, json.dumps(address))
+            my_stat.count('geocode_ok')
         except Exception as e:
             print str(e)
             village = "{%s}" % (key)    # enclose in curly braces so that users *see* it's a token
+            my_stat.count('geocode_failed')
 
     else:
         raw = json.loads(val)
         if raw:
             cached = True
             village = raw[0]['formatted']
+            my_stat.count('cache_ok')
         else:
             village = "{%s}" % (key)    # enclose in curly braces so that users *see* it's a token
+            my_stat.count('cache_failed')
     
     addr = {
         'village' : village,
